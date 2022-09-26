9336a1c1c3644804bf1ef668f4cab47c5739953b
ozturkbasrii <ozturkbasrii@gmail.com>
2022-09-26 13:26:36 +0300

    initial commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Hasan Basri Öztürk on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
